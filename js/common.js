1e06c3e6eb9ae06abc95e2740a08b682397044d4
Deddy Kakunsi <deddykakunsi@outlook.com>
2015-08-07 19:14:52 +0800

    Tambah get nama bulan.
    Simpan basic halaman login.

--- js/common.js
+++ js/common.js
@@ -722,7 +722,31 @@ var myDate = {
 				case 12: return 'December'
 			}
 		},
-		
+
+		getNama: function ( index ) {
+
+			if ( index > 12 )
+				index -= 12;
+			
+			if ( index < 1 )
+				index += 12;
+
+			switch ( index ) {
+				case 1: return 'JANUARI'
+				case 2: return 'FEBRUARI'
+				case 3: return 'MARET'
+				case 4: return 'APRIL'
+				case 5: return 'MEI'
+				case 6: return 'JUNI'
+				case 7: return 'JULI'
+				case 8: return 'AGUSTUS'
+				case 9: return 'SEPTEMBER'
+				case 10: return 'OKTOBER'
+				case 11: return 'NOVEMBER'
+				case 12: return 'DESEMBER'
+			}
+		},
+				
 		getRealName: function ( name ) {
 			
 			name = name.toLowerCase ();
